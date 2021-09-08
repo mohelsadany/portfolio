db6d04c1d81b82349270f63218c8665f59708cbd
Mohamed Ibrahim Elsadany <36929282+mohelsadany@users.noreply.github.com>
2021-09-08 16:28:33 +0200

    remove Tawk chat

--- projects/script.js
+++ projects/script.js
@@ -87,15 +87,15 @@ getProjects().then(data => {
 // fetch projects end
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6138955c25797d7a89fde1db/1ff2get4m';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6138955c25797d7a89fde1db/1ff2get4m';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 // End of Tawk.to Live Chat
 
 // disable developer mode
